6eb09ee32d5e2977aa575d20ef3d5bf8901a53ab
athar <athar.adel@gmx.de>
2021-06-05 15:52:19 +0200

    Test pull function

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public  class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
         System.out.println("Athar Branch!");
-    
+        System.out.println("Master Branch!");
     }
 }
