f923032603b52694ecab4bc7c6a95041162a1c1c
athar <athar.adel@gmx.de>
2021-06-05 19:16:29 +0200

    added Header Labels

--- src/Views/Components/Label.java
+++ src/Views/Components/Label.java
@@ -1,7 +1,9 @@
 package Views.Components;
 import java.awt.Font;
-
+import java.awt.Component;
 import javax.swing.*;
+
+import Views.MasterGUI;
 
 public class Label extends JLabel{
   /*
@@ -11,13 +13,22 @@ public class Label extends JLabel{
   private boolean isHeader = false;
   public Label() {
     super();
-    setFont(new Font("Arial", Font.PLAIN, 15));
+    setFont(MasterGUI.basicFont);
     setSize(250, 25);
   }
   public Label(int x, int y, String text) {
     super(text);
-    setFont(new Font("Arial", Font.PLAIN, 15));
+    setFont(MasterGUI.basicFont);
     setBounds(x, y, 250, 25);
+  }
+  public Label(String text, Component cmp) {
+    super(text);
+    setFont(MasterGUI.basicFont);
+    setBounds(cmp.getX() + cmp.getWidth() + 12, cmp.getY(), 250, cmp.getHeight());
+  }
+
+  public Label(ImageIcon image) {
+    super(image);
   }
   public void setEditable(){
     isEditable = true;
@@ -25,6 +36,14 @@ public class Label extends JLabel{
   public boolean getEditable(){
     return isEditable;
   }
-
+  public void setHeader(){
+    isHeader = true;
+    isEditable = true;
+    setSize(500,40);
+    setFont(getFont().deriveFont(Font.BOLD, 30f));
+  }
+  public boolean getHeader(){
+    return isHeader;
+  }
   
 }
