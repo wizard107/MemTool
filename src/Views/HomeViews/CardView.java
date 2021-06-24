cee7a2347515e5d61c74a213c0c0013e7ffedb4b
athar <athar.adel@gmx.de>
2021-06-24 19:37:11 +0200

    Completed Basic Functionality of CardView

--- src/Views/HomeViews/CardView.java
+++ src/Views/HomeViews/CardView.java
@@ -11,6 +11,8 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 import java.awt.color.*;
 import Model.Deck;
 import Views.MainGUI;
@@ -25,13 +27,18 @@ import Views.Components.TextPane;
 public class CardView extends Panel{
     private static JFrame frame;
     private Point point = new Point(50 , 50);
+    private Point point2 = new Point(50,100);
     private Deck deck;
     private static Panel superPanel;
     private static Panel subPanel;
-    private int currentID = -1;
-    private static int count = 0;
-    private String[] questions = new String[11];
-    private String[] answers = new String[11];
+    private int currentID = -1; //if currentID of card is -1 it is not saved, otherwise saves the id of currentcard
+    private static int deckSize = 9;
+    private int cardSpace = 35;
+    //private String[] questions = new String[15];
+    //private String[] answers = new String[15];
+    private List<String> questions = new ArrayList<String>();
+    private List<String> answers = new ArrayList<String>();
+    private Button save;
     TextPane questionPanelQA;
     TextPane answerPanelQA;
     private static JScrollPane scroller;
@@ -40,8 +47,10 @@ public class CardView extends Panel{
         this.frame = frame;
         this.deck  = deck;
         for(int i=0;i<10;i++){
-            questions[i] = "question" + String.valueOf(i);
-            answers[i] = "answer" + String.valueOf(i);
+            //questions[i] = "question" + String.valueOf(i);
+            //answers[i] = "answer" + String.valueOf(i);
+            questions.add(i,"question" + String.valueOf(i));
+            answers.add(i,"question" + String.valueOf(i));
         }
         superPanel = new Panel();
         superPanel.setPreferredSize(new Dimension(frame.getWidth()/2 + 70, frame.getHeight()*2+50));
@@ -54,14 +63,16 @@ public class CardView extends Panel{
         warning2.setSize(1000, 50);
         Label warning3 = new Label(20, 360, "SCROLL DOWN TO EDIT", MasterGUI.purple, 30f);
         warning3.setSize(350, 50);
-        drawCards();
+        drawRightSide();
         scroller = makeScroller(superPanel);
         //add(warning1);
        // add(warning2);
         //add(warning3);
+        add(save);
         add(questionPanelQA);
         add(answerPanelQA);
         add(subPanel);
+        
         
         add(scroller);
         createWarning();
@@ -69,103 +80,167 @@ public class CardView extends Panel{
         //frame.setBackground(MasterGUI.babyred);
         
     }
-    private void drawCards(){
+    private void drawRightSide(){
+        
         questionPanelQA = new TextPane(point.x + 630, point.y - 40,500, 270);
         answerPanelQA = new TextPane(point.x + 630, point.y + 260, 500, 270);
         questionPanelQA.setText("edit your question");
         answerPanelQA.setText("edit your answer");
-        //questionPanelQA.setBounds(point.x + 600, point.y - 40, 500, 270);
-        //answerPanelQA.setBounds(point.x + 600, point.y + 260, 500, 270);
         questionPanelQA.setBorder(BorderFactory.createLineBorder(Color.black));
         answerPanelQA.setBorder(BorderFactory.createLineBorder(Color.black));
-        Point point2 = new Point(50,100);
+        save = new Button(point.x+630, point2.y+183 ,"SAVE", MasterGUI.black_gray,80,25);   
+        createCardViewBtn(save);
+        saveQA(save);
+        drawLeftSide();
+        
+    }
+    public void drawLeftSide(){
+        Button addBtn = new Button(point.x+505, point.y+10,"ADD", MasterGUI.yellow,80,25);   //später counter für actionlistener, der immer um eins hochgeht, jeder edit übernimmt dann die kontrolle für deck[count++]
+        addBtn.setFont(MasterGUI.poppinsFont.deriveFont(15f));
+        createCardViewBtn(addBtn);
+        addQA(addBtn);
         Panel idPanel = new Panel();
+        idPanel.setBounds(point2.x-40, point2.y-40, 385,25);
+        idPanel.setBorder(BorderFactory.createLineBorder(Color.black));
         Label title = new Label(point2.x-45, point2.y-90, " TITLE", MasterGUI.black,25f);
         title.setSize(500, 50);
-        idPanel.setBounds(point2.x-40, point2.y-40, 385,25);
         Label idlabel = new Label(point2.x-40, point2.y-40, " ID", MasterGUI.black,15f);
-        idlabel.setSize(35, 25);
+        createCardViewLabel(idlabel,35, 25);
         idlabel.setBorder(BorderFactory.createLineBorder(Color.black));
         Label namelabel = new Label(point2.x-5, point2.y-40, " NAME", MasterGUI.black,15f);
-        namelabel.setSize(250, 25);
-        namelabel.setBorder(BorderFactory.createLineBorder(Color.black));
+        createCardViewLabel(namelabel,250, 25);
         Label duelabel = new Label(point2.x+245, point2.y-40, " DUEDATE", MasterGUI.black,15f);
-        duelabel.setSize(100, 25);
-        duelabel.setBorder(BorderFactory.createLineBorder(Color.black));
-        //Label decklabel = new Label(point2.x+345, point2.y-40, " DECK", MasterGUI.black,15f);
-        //decklabel.setSize(155, 25);
-        //decklabel.setBorder(BorderFactory.createLineBorder(Color.black));
-        idPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        createCardViewLabel(duelabel, 100, 25);
+        
         superPanel.add(title);
         superPanel.add(idlabel);
         superPanel.add(namelabel);
         superPanel.add(duelabel);
+        superPanel.add(addBtn);
         //superPanel.add(decklabel);
         superPanel.add(idPanel);
-        subPanel.add(questionPanelQA);
-        subPanel.add(answerPanelQA);
+        //subPanel.add(save);
+        //subPanel.add(questionPanelQA);
+        //subPanel.add(answerPanelQA);
         //superPanel.add(questionPanelQA);
         //superPanel.add(answerPanelQA);
-        int cardSpace = 35;
-        for(int i = 0;i<10;i++){
+        for(int i = 0;i<questions.size();i++){
             Button edit = new Button(point2.x+505, point2.y-40 + cardSpace,"EDIT", MasterGUI.purple,80,25);   //später counter für actionlistener, der immer um eins hochgeht, jeder edit übernimmt dann die kontrolle für deck[count++]
-            edit.setFont(MasterGUI.poppinsFont.deriveFont(15f));
-            edit.setForeground(Color.WHITE);
-            edit.setFocusPainted(false);
-            edit.setContentAreaFilled(true);
-            edit.setMargin(new Insets(5, 5, 3, 3));
+            createCardViewBtn(edit);
             switchQA(edit, i);
-            Button save = new Button(point2.x+400, point2.y-40 + cardSpace,"SAVE", MasterGUI.yellow,80,25);   
-            save.setFont(MasterGUI.poppinsFont.deriveFont(15f));
-            save.setForeground(Color.WHITE);
-            save.setFocusPainted(false);
-            save.setContentAreaFilled(true);
-            save.setMargin(new Insets(5, 5, 3, 3));
-            saveQA(save, i);
+            Button delete = new Button(point2.x+400, point2.y-40 + cardSpace,"DELETE", MasterGUI.red,80,25);   
+            createCardViewBtn(delete);
+            deleteQA(delete, i);
             idPanel = new Panel();
             idPanel.setBounds(point2.x-40, point2.y-40+ cardSpace, 385,25);
             idlabel = new Label(point2.x-40, point2.y-40+ cardSpace, " IDT", MasterGUI.black,15f);
-            idlabel.setSize(35, 25);
-            idlabel.setBorder(BorderFactory.createLineBorder(Color.black));
+            createCardViewLabel(idlabel,35, 25);
             namelabel = new Label(point2.x-5, point2.y-40+ cardSpace, " NAMET", MasterGUI.black,15f);
-            namelabel.setSize(250, 25);
-            namelabel.setBorder(BorderFactory.createLineBorder(Color.black));
+            createCardViewLabel(namelabel, 250, 25);
             duelabel = new Label(point2.x+245, point2.y-40+ cardSpace, " DUEDATET", MasterGUI.black,15f);
-            duelabel.setSize(100, 25);
-            duelabel.setBorder(BorderFactory.createLineBorder(Color.black));
+            createCardViewLabel(duelabel,100, 25);
             idPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-            superPanel.add(save);
+            //superPanel.add(save);
+            superPanel.add(delete);
             superPanel.add(idlabel);
             superPanel.add(namelabel);
             superPanel.add(duelabel);
-           // superPanel.add(decklabel);
             superPanel.add(idPanel);
             superPanel.add(edit);
             cardSpace+=35;
-            count++;
         }
     }
+    public static void createCardViewBtn(Button btn){
+        btn.setFont(MasterGUI.poppinsFont.deriveFont(15f));
+        btn.setForeground(Color.WHITE);
+        btn.setFocusPainted(false);
+        btn.setContentAreaFilled(true);
+        btn.setMargin(new Insets(5, 5, 3, 3));
+    }
 
+    public static void createCardViewLabel(Label label, int w, int h){
+        label.setSize(w, h);
+        label.setBorder(BorderFactory.createLineBorder(Color.black));
+    }
+    private void addQA(Button addBtn){
+        ActionListener adding = new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                questionPanelQA.removeAll();
+                answerPanelQA.removeAll();
+                questionPanelQA.setText("New Card Added");
+                answerPanelQA.setText("New Card Added");
+                deckSize++;
+                currentID = deckSize;
+                questions.add(deckSize, "");
+                answers.add(deckSize, "");
+                Panel idPanel = new Panel();
+                idPanel.setBounds(point2.x-40, point2.y-40+ cardSpace, 385,25);
+                idPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+                Label idlabel = new Label(point2.x-40, point2.y-40+ cardSpace, " IDT", MasterGUI.black,15f);
+                createCardViewLabel(idlabel,35, 25);
+                Label namelabel = new Label(point2.x-5, point2.y-40+ cardSpace, " NAMET", MasterGUI.black,15f);
+                createCardViewLabel(namelabel,250, 25);
+                Label duelabel = new Label(point2.x+245, point2.y-40+ cardSpace, " DUEDATET", MasterGUI.black,15f);
+                createCardViewLabel(duelabel,100, 25);
+                
+                superPanel.add(idlabel);
+                superPanel.add(namelabel);
+                superPanel.add(duelabel);
+                superPanel.add(idPanel);
+                Button edit = new Button(point2.x+505, point2.y-40 + cardSpace,"EDIT", MasterGUI.purple,80,25);   //später counter für actionlistener, der immer um eins hochgeht, jeder edit übernimmt dann die kontrolle für deck[count++]
+                createCardViewBtn(edit);
+                Button delete = new Button(point2.x+400, point2.y-40 + cardSpace,"DELETE", MasterGUI.red,80,25);   
+                createCardViewBtn(delete);
+                deleteQA(delete, deckSize);
+                switchQA(edit, deckSize);
+                superPanel.add(edit);
+                superPanel.add(delete);
+                superPanel.repaint();
+                cardSpace+=35;
+                System.out.println("Added"); 
+            }
+        };
+        addBtn.addActionListener(adding);
+    }
+    private void deleteQA(Button deleteBtn, int i){
+        ActionListener deletion = new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                questionPanelQA.removeAll();
+                answerPanelQA.removeAll();
+                questions.remove(i);
+                answers.remove(i);
+                superPanel.removeAll();
+                cardSpace = 35;
+                deckSize--;
+                currentID = -1;
+                drawLeftSide();
+                superPanel.repaint();
+                System.out.println("Deleted");
+            }
+        };
+        deleteBtn.addActionListener(deletion);
+    }
     private void switchQA(Button editBtn, int i){
         ActionListener change = new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 questionPanelQA.removeAll();
                 answerPanelQA.removeAll();
-                questionPanelQA.setText(questions[i]);
-                answerPanelQA.setText(answers[i]);
+                questionPanelQA.setText(questions.get(i));
+                answerPanelQA.setText(answers.get(i));
                 currentID = i;
                 System.out.println("Edit"); 
             }
         };
         editBtn.addActionListener(change);
     }
-    private void saveQA(Button saveBtn, int i){
+    private void saveQA(Button saveBtn){
         ActionListener saveInput = new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 if(currentID==-1) System.out.println("Not Saved");
                 else{
-                    questions[currentID] = questionPanelQA.getText();
-                    answers[currentID] = answerPanelQA.getText();    
+                    questions.set(currentID, questionPanelQA.getText()); //<---set
+                    answers.set(currentID, answerPanelQA.getText());
+                    //answers[currentID] = answerPanelQA.getText();    
                     System.out.println("Saved");          
                 }
             }
